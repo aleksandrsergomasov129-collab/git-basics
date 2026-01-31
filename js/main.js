3a44007fe53a941eef34691cc0488e6bef93619c
Aleksandr Sergomasov <aleksandrsergomasov129@gmail.com>
2026-01-31 13:57:50 +0300

    changed js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,21 @@
-$(function(){
+$(function () {
 
-    let showPopup = function(){
+    let showPopup = function () {
         $('.popup-container').css('display', 'flex');
     };
 
-    let closePopup = function(){
+    let closePopup = function () {
         $('.popup-container').css('display', 'none');
     };
-
-    let clearForm = function(){
-        $('.popup-container > input[type="text"]').val('');
+    let clearForm = function () {
+        $('#name').val('');
+        $('#phone').val('');
     };
 
-    $('.close').on('click', function(){
+    $('button').on('click', function () {
+        showPopup();
+    });
+    $('.close').on('click', function () {
         clearForm();
         closePopup();
     });
